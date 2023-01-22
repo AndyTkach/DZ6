fea9cb6e2afa1aa4dc38959bfe3a756e1a020422
AndyTkach <andrutka4@gmai.com>
2023-01-22 22:13:28 +0300

    Listo

---
+++ 41/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задание 41
+
+Console.Write("Введите элементы: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+
+string mas= String.Concat<int>(arr);
+ 
+Console.WriteLine($"{mas} -> {count}");
---
+++ 43/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 43
+
+Console.Write("Введите b1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+ 
+ 
+var x = -(k1 - k2) / (b1 - b2);
+var y = b1 * x + k1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x};{y})");
+
